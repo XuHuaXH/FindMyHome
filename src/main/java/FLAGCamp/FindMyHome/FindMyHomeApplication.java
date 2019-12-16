4202ed6954f3fba8b8765d6509edcd8ddf8f052a
Hua Xu <amyxuhua@gmail.com>
2019-12-16 16:23:20 +0800

    initial project setup

---
+++ src/main/java/FLAGCamp/FindMyHome/FindMyHomeApplication.java
@@ -0,0 +1,13 @@
+package FLAGCamp.FindMyHome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindMyHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindMyHomeApplication.class, args);
+	}
+
+}
