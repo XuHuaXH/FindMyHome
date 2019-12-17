eff914403139daf8e2781fc09d8a4ea720018667
0d2_100004383376_zixiu <0d2_100004383376_zixiu@git.cloud.tencent.com>
2019-12-16 22:38:32 -0500

    Add some fake api

--- src/main/java/FLAGCamp/FindMyHome/controller/FakeController.java
+++ src/main/java/FLAGCamp/FindMyHome/controller/FakeController.java
@@ -1,47 +1,32 @@
 package FLAGCamp.FindMyHome.controller;
 
-import FLAGCamp.FindMyHome.model.Property;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collection;
 
 @RestController
 @RequestMapping("/fake")
 public class FakeController {
     @GetMapping("/search")
-    public String search(@RequestParam(value = "keyword", required = false) String address ){
-        return "{ \n" +
-                "  \"totalResult\": 1,    \n" +
-                "  \"SearchLat\": 37.802452, \n" +
-                "  \"SearchLong\" :-122.417227,\n" +
-                "  \"results\" : [   \n" +
-                "    {\n" +
-                "      \"uuid\" : \"1\",\n" +
-                "      \"Address\" : \"950 Lombard St, San Francisco, CA 94133\",\n" +
-                "      \"Lat\" : 37.802452,\n" +
-                "      \"Long\" : -122.417227,\n" +
-                "      \"Price\": 40500000,\n" +
-                "      \"Bedroom\" : 6,\n" +
-                "      \"Bathroom\" : 8,\n" +
-                "      \"area\" : 9495,\n" +
-                "      \"Like\": false\n" +
-                "    },\n" +
-                "  {\n" +
-                "      \"uuid\" : \"2\",\n" +
-                "      \"Address\" : \"2698 Pacific Ave, San Francisco, CA 94115\",\n" +
-                "      \"Lat\" : 37.793122,\n" +
-                "      \"Long\" : -122.439469,\n" +
-                "      \"Price\": 26800000,\n" +
-                "      \"Bedroom\" : 6,\n" +
-                "      \"Bathroom\" : 8,\n" +
-                "      \"area\" : 10734,\n" +
-                "      \"Like\": false\n" +
-                "    }\n" +
-                "]\n" +
-                "}\n";
+    public String search(@RequestParam(value = "keyword", required = false) String keyword ){
+        return "{\"totalResult\":3,\"SearchLat\":37.802452,\"SearchLong\":-122.417227,\"SearchKeyWord\": \"" + keyword + "\",\"results\":[{\"uuid\":\"1\",\"Address\":\"950 Lombard St, San Francisco, CA 94133\",\"Lat\":37.802452,\"Long\":-122.417227,\"Price\":40500000,\"Bedroom\":6,\"Bathroom\":8,\"area\":9495,\"Like\":true},{\"uuid\":\"2\",\"Address\":\"2698 Pacific Ave, San Francisco, CA 94115\",\"Lat\":37.793122,\"Long\":-122.439469,\"Price\":26800000,\"Bedroom\":6,\"Bathroom\":8,\"area\":10734,\"Like\":false},{\"uuid\":\"3\",\"Address\":\"870 Harrison St UNIT 305, San Francisco, CA 94107\",\"Lat\":37.779807,\"Long\":-122.401351,\"Price\":466345,\"Bedroom\":2,\"Bathroom\":1,\"area\":889,\"Like\":false}]}";
+    }
+
+    @GetMapping("/liked")
+    public String liked(){
+        return "{\"totalResult\":3,\"SearchLat\":37.802452,\"SearchLong\":-122.417227,\"SearchKeyWord\":\"San Francisco\",\"results\":[{\"uuid\":\"1\",\"Address\":\"950 Lombard St, San Francisco, CA 94133\",\"Lat\":37.802452,\"Long\":-122.417227,\"Price\":40500000,\"Bedroom\":6,\"Bathroom\":8,\"area\":9495,\"Like\":true},{\"uuid\":\"2\",\"Address\":\"2698 Pacific Ave, San Francisco, CA 94115\",\"Lat\":37.793122,\"Long\":-122.439469,\"Price\":26800000,\"Bedroom\":6,\"Bathroom\":8,\"area\":10734,\"Like\":false},{\"uuid\":\"3\",\"Address\":\"870 Harrison St UNIT 305, San Francisco, CA 94107\",\"Lat\":37.779807,\"Long\":-122.401351,\"Price\":466345,\"Bedroom\":2,\"Bathroom\":1,\"area\":889,\"Like\":false}]}";
+    }
+
+    @GetMapping("/history")
+    public String history(){
+        return "{\"totalResult\":1,\"mode\":\"History\",\"results\":[{\"uuid\":\"2\",\"Address\":\"2698 Pacific Ave, San Francisco, CA 94115\",\"Lat\":37.793122,\"Long\":-122.439469,\"Price\":26800000,\"MainPic\":\"2.jpg\",\"Bedroom\":6,\"Bathroom\":8,\"area\":10734,\"Like\":false}]}";
+    }
+
+    @GetMapping("/owned")
+    public String owned(){
+        return "{\"totalResult\":1,\"mode\":\"Owned\",\"results\":[{\"uuid\":\"3\",\"Address\":\"870 Harrison St UNIT 305, San Francisco, CA 94107\",\"Lat\":37.779807,\"Long\":-122.401351,\"Price\":466345,\"MainPic\":\"3.jpg\",\"Bedroom\":2,\"Bathroom\":1,\"area\":889,\"Like\":false}]}";
     }
 
 }
