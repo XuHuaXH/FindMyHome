c716e2d604d8922125d71217eefda2c265cad47d
Hua Xu <amyxuhua@gmail.com>
2019-12-18 18:58:51 +0800

    added fake APIs for /route and /travel-time-estimation

--- src/main/java/FLAGCamp/FindMyHome/controller/FakeController.java
+++ src/main/java/FLAGCamp/FindMyHome/controller/FakeController.java
@@ -1,9 +1,6 @@
 package FLAGCamp.FindMyHome.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -31,5 +28,123 @@ public class FakeController {
     public String owned(){
         return "{\"totalResult\":1,\"mode\":\"Owned\",\"results\":[{\"uuid\":\"3\",\"Address\":\"870 Harrison St UNIT 305, San Francisco, CA 94107\",\"Lat\":37.779807,\"Long\":-122.401351,\"Price\":466345,\"MainPic\":\"3.jpg\",\"Bedroom\":2,\"Bathroom\":1,\"area\":889,\"Like\":false}]}";
     }
+
+    @GetMapping("/route")
+    public String getRoutes() {
+        return "{\n" +
+                "  \"routes\": [\n" +
+                "    {\n" +
+                "      \"id\": 5,\n" +
+                "      \"name\": \"preloaded route\",\n" +
+                "      \"departureTime\": \"08:00\",\n" +
+                "      \"days\": [\n" +
+                "        \"Monday\",\n" +
+                "        \"Friday\",\n" +
+                "        \"Wednesday\"\n" +
+                "      ],\n" +
+                "      \"travelModes\": [\n" +
+                "        \"driving\"\n" +
+                "      ],\n" +
+                "      \"nodes\": [\n" +
+                "        {\n" +
+                "          \"id\": 6,\n" +
+                "          \"name\": \"home\",\n" +
+                "          \"address\": {\n" +
+                "            \"id\": 2,\n" +
+                "            \"streetNo\": 950,\n" +
+                "            \"roadName\": \"Lombard St\",\n" +
+                "            \"city\": \"San Francisco\",\n" +
+                "            \"state\": \"CA\",\n" +
+                "            \"country\": \"US\",\n" +
+                "            \"zipCode\": \"94133\",\n" +
+                "            \"latitude\": 37.802452,\n" +
+                "            \"longitude\": -122.417227,\n" +
+                "            \"zone\": 0\n" +
+                "          }\n" +
+                "        },\n" +
+                "        {\n" +
+                "          \"id\": 7,\n" +
+                "          \"name\": \"work\",\n" +
+                "          \"address\": {\n" +
+                "            \"id\": 4,\n" +
+                "            \"streetNo\": 950,\n" +
+                "            \"roadName\": \"Lombard St\",\n" +
+                "            \"city\": \"San Francisco\",\n" +
+                "            \"state\": \"CA\",\n" +
+                "            \"country\": \"US\",\n" +
+                "            \"zipCode\": \"94133\",\n" +
+                "            \"latitude\": 37.802452,\n" +
+                "            \"longitude\": -122.417227,\n" +
+                "            \"zone\": 37\n" +
+                "          }\n" +
+                "        }\n" +
+                "      ]\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}";
+    }
+
+    @PostMapping("/route")
+    public String postRoute() {
+        return "{\"status\" : \"OK\"}";
+    }
+
+    @DeleteMapping("/route")
+    public String deleteRoute() {
+        return "{\"status\" : \"OK\"}";
+    }
+
+    @GetMapping("estimate-travel-time")
+    public String estimateTravelTime() {
+        return "{\n" +
+                "  \"nodes\": [\n" +
+                "    {\n" +
+                "      \"id\": 7,\n" +
+                "      \"name\": \"home\",\n" +
+                "      \"address\": {\n" +
+                "        \"id\": 2,\n" +
+                "        \"streetNo\": 950,\n" +
+                "        \"roadName\": \"Lombard St\",\n" +
+                "        \"city\": \"San Francisco\",\n" +
+                "        \"state\": \"CA\",\n" +
+                "        \"country\": \"US\",\n" +
+                "        \"zipCode\": \"94133\",\n" +
+                "        \"latitude\": 37.802452,\n" +
+                "        \"longitude\": -122.417227,\n" +
+                "        \"zone\": 0\n" +
+                "      }\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"id\": 6,\n" +
+                "      \"name\": \"work\",\n" +
+                "      \"address\": {\n" +
+                "        \"id\": 2,\n" +
+                "        \"streetNo\": 950,\n" +
+                "        \"roadName\": \"Lombard St\",\n" +
+                "        \"city\": \"San Francisco\",\n" +
+                "        \"state\": \"CA\",\n" +
+                "        \"country\": \"US\",\n" +
+                "        \"zipCode\": \"94133\",\n" +
+                "        \"latitude\": 37.802452,\n" +
+                "        \"longitude\": -122.417227,\n" +
+                "        \"zone\": 0\n" +
+                "      }\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"travel_mode\": [\n" +
+                "    \"transit\"\n" +
+                "  ],\n" +
+                "  \"optimistic_time\": [\n" +
+                "    21\n" +
+                "  ],\n" +
+                "  \"pessimistic_time\": [\n" +
+                "    40\n" +
+                "  ],\n" +
+                "  \"fare\": [\n" +
+                "    10\n" +
+                "  ]\n" +
+                "}";
+    }
+
 
 }
